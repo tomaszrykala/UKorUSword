8cd36643627f9bff7e5deeca4a763e45b9b343d9
tomaszrykala <tomasz.a.rykala@gmail.com>
2023-08-16 16:43:29 +0100

    Add basic logic and test words

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final testWords = [
+      Word(word: "pavement", locale: Locale.UK),
+      Word(word: "sidewalk", locale: Locale.US),
+      Word(word: "'ground' <- beef", locale: Locale.US),
+      Word(word: "turkey -> 'mince'", locale: Locale.UK),
+    ];
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -31,13 +41,16 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        words: testWords,
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title, required this.words});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -49,22 +62,32 @@ class MyHomePage extends StatefulWidget {
   // always marked "final".
 
   final String title;
+  final List<Word> words;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState(words);
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _scoreCounter = 0;
+  List<Word> _words = [];
 
-  void _incrementCounter() {
+  _MyHomePageState(List<Word> words) {
+    _words = words;
+  }
+
+  void _updateScore(bool guessed) {
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      if (guessed) {
+        _scoreCounter++;
+      } else {
+        _scoreCounter = 0;
+      }
     });
   }
 
@@ -72,54 +95,90 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
+    Word word = _words.random();
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Container(
+                margin: const EdgeInsets.only(bottom: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'The term is:\n${word.word}',
+                      style: Theme.of(context).textTheme.headlineLarge,
+                    )
+                  ],
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                    margin: const EdgeInsets.all(16),
+                    child: buildMaterialButton(word, Locale.UK)),
+                Container(
+                    margin: const EdgeInsets.all(16),
+                    child: buildMaterialButton(word, Locale.US))
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Container(
+                margin: const EdgeInsets.only(top: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Your score is: $_scoreCounter',
+                      style: Theme.of(context).textTheme.headlineSmall,
+                    )
+                  ],
+                ))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  MaterialButton buildMaterialButton(Word word, Locale locale) {
+    var color = locale == Locale.UK ? Colors.redAccent : Colors.lightBlueAccent;
+    return MaterialButton(
+      height: 80,
+      color: color,
+      onPressed: () {
+        checkGuess(word, locale);
+      },
+      child: Text(locale.name),
+    );
+  }
+
+  void checkGuess(Word word, Locale locale) {
+    _updateScore(word.locale == locale);
+  }
+}
+
+enum Locale { US, UK }
+
+final class Word {
+  String word;
+  Locale locale;
+  bool seen = false;
+
+  Word({required this.word, required this.locale});
+}
+
+extension WordListRandomPick on List<Word> {
+  Word random() {
+    if (isNotEmpty) {
+      int index = Random().nextInt(length);
+      return this[index];
+    } else {
+      return Word(word: "Error", locale: Locale.UK);
+    }
   }
 }
